5afd02d71592f00440d4dd3dc06daf2de29364ef
robin <robin@ansvia.com>
2017-11-24 20:14:20 +0700

    add readme and license files

---
+++ README.md
@@ -0,0 +1,8 @@
+Document File Reader
+=======================
+
+Simple Rust library to extract readable text from specific document format like docx.
+
+
+
+ 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+/**
+ * Copyright 2017 Robin Syihab. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
 
 #![allow(unused_imports, dead_code, unused_must_use)]
 
@@ -28,8 +49,6 @@ impl Docx {
         let mut archive = ZipArchive::new(file)?;
 
         let mut xml_data = String::new();
-
-//        let doc_xml_file:File;
 
         for i in 0..archive.len(){
             let mut c_file = archive.by_index(i).unwrap();
